cc48804cb1f42c2eef9dcf508d34f843cb8a3cb7
ankitojha07 <ankitojha1409@gmail.com>
2024-08-25 12:04:46 +0530

    created a responsive navbar for our apple website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,15 @@ const Navbar = () => {
         <img src={appleImg} alt="" width={14} height={18} />
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
-            <div key={nav} className="px-5">
+            <div
+              key={nav}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+            >
               {nav}
             </div>
           ))}
         </div>
-        <div>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
           <img src={searchImg} alt="Search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
         </div>
